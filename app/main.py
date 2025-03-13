7f00d3efc0a6ae827069198fe47f56fc1aadbd09
JacsonAnderson <jacson11anderson.jsn@gmail.com>
2025-03-12 22:26:12 -0300

    Criamos a tabela, e salvei o comando para futuramente automatizar o processo... e tambem trabalhamos um pouco na estrutura html

--- app/main.py
+++ app/main.py
@@ -11,6 +11,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")  # Certifique-se de ter esse template em templates/index.html
 
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")  # Certifique-se de ter o dashboard.html em app/templates
+
+
+@app.route("/manage_channels")
+def manage_channels():
+    return render_template("manage_channels.html")  # Renderiza o manage_channels.html
+
 def open_browser():
     webbrowser.open_new("http://localhost:1313")
 
